ec30ee9136c5e0990e035877855eae88f435efb0
Roller Davic <sonataruby@gmail.com>
2022-04-03 11:44:13 +0700

    Update Clickoffer.php

--- app/Controllers/Clickoffer.php
+++ app/Controllers/Clickoffer.php
@@ -17,10 +17,35 @@ class Clickoffer extends BaseController
         $this->click = new ClickModel;
         $this->offerfinish = new OfferFinishModel;
     }
+
+    private function get_ip_address()
+    {
+        foreach (array('HTTP_CLIENT_IP',
+                       'HTTP_X_FORWARDED_FOR',
+                       'HTTP_X_FORWARDED',
+                       'HTTP_X_CLUSTER_CLIENT_IP',
+                       'HTTP_FORWARDED_FOR',
+                       'HTTP_FORWARDED',
+                       'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true){
+                foreach (explode(',', $_SERVER[$key]) as $IPaddress){
+                    $IPaddress = trim($IPaddress); // Just to be safe
+
+                    if (filter_var($IPaddress,
+                                   FILTER_VALIDATE_IP,
+                                   FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)
+                        !== false) {
+
+                        return $IPaddress;
+                    }
+                }
+            }
+        }
+    }
     public function index($id, $auth_id)
     {
         
-        $ip = $this->request->getIPAddress();
+        $ip = $this->get_ip_address();
         $this->client = json_decode(file_get_contents("http://ip-api.com/json/".$ip));
         $useragent = $this->request->getUserAgent();
         $this->offer->join("offer_api","offer_api.id=api_id","left");
